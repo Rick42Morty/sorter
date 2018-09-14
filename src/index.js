c20188cb101338883d8386e3bc532805e945fbe5
Rick42Morty <evgenevgen00@mail.ru>
2018-09-14 11:05:18 +0300

    Implementes all the methods required

--- src/index.js
+++ src/index.js
@@ -1,31 +1,54 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.arr = [];
+    this.sortFunc = (a, b) => a - b;
   }
 
   add(element) {
     // your implementation
+    this.arr.push(element);
   }
 
   at(index) {
     // your implementation
+    return this.arr[index];
   }
 
   get length() {
     // your implementation
+    return this.arr.length;
   }
 
   toArray() {
     // your implementation
+    return this.arr;
   }
 
   sort(indices) {
     // your implementation
+    var subArr = [];
+    indices.sort();
+
+    for (var i in indices) {
+      subArr.push(this.arr[indices[i]]);
+    }
+    
+    subArr.sort(this.sortFunc);
+    
+    var count = 0;
+
+    for (var i in indices) {
+      this.arr[indices[i]] = subArr[count];
+      count++;
+    }
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.sortFunc = compareFunction;
   }
+  
 }
+
 
 module.exports = Sorter;
